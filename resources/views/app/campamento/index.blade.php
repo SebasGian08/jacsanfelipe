dd41270d25a42388fbaec113527fc846659872f2
SebasGian08 <sebas_vasquez_1234@hotmail.com>
2025-01-04 10:36:35 -0500

    cambio

--- resources/views/app/campamento/index.blade.php
+++ resources/views/app/campamento/index.blade.php
@@ -15,26 +15,27 @@
     <div class="form-register">
         <form class="form-login" action="{{ route('campamento.store') }}" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="content_titulo_login" style="text-align: center; padding: 20px; background: linear-gradient(135deg, #007bff, #00c6ff); border-radius: 15px; box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2); color: white;">
-                <img src="{{ asset('app/img/logorj.png') }}" alt="Logo JAC" style="width: 300px;"><br><br>
-                <span style="margin-top:30px !important;font-size: 2.8em; font-weight: bold;color:white;">CAMPAMENTO IE 2025</span>
-                <p style="margin-top:30px; font-size: 35px !important; font-weight: bold; color: #24303c;">
-                    <i class="fas fa-money-bill-wave" style="vertical-align: middle; margin-right: 5px;"></i>
-                    Colaboración: <strong style="font-size: 1.2em;">S/200.00</strong>
-                </p>
+            <div class="content_titulo_login" style="text-align: center; padding: 15px;color: white;">
+                <img src="{{ asset('app/img/logorj.png') }}" alt="Logo JAC" style="width: 200px; margin-bottom: 10px;">
+                <h2 style="font-size: 1.8em; margin: 5px 0;">CAMPAMENTO IE 2025</h2>
+                <p style="font-size: 1.2em; margin: 5px 0;">Colaboración: <strong>S/200.00</strong></p>
             </div>
-             <br><br>
+
+
+            <br><br>
             <input type="hidden" name="id_nuevo" id="id_nuevo" value="10" required>
             <div class="form-group">
-                <label for="nombres" class="text-primary-m">Nombre y Apellido</label>
+                <label for="nombres" class="text-primary-m" style="color:rgba(0, 195, 244, 1) !important;">Nombre y
+                    Apellido</label>
                 <input type="text" autocomplete="off" id="nombres" name="nombre"
                     class="controls {{ $errors->has('nombre') ? ' is-invalid' : '' }}" value="{{ old('nombre') }}"
                     required placeholder="Ingrese Nombre y Apellido">
             </div>
             <br>
             <div class="form-group col-12">
-                <label for="celulas" class="text-primary-m">RED JUVENIL A LA QUE ASISTES</label> <small
-                    class="form-text text-muted" style="font-size: 20px; color:white;">
+                <label for="celulas" class="text-primary-m" style="color:rgba(0, 195, 244, 1) !important;">RED JUVENIL
+                    A LA QUE
+                    ASISTES</label> <small class="form-text text-muted" style="font-size: 20px; color:white;">
                     (Si no asistes a ninguna red, seleccione <strong>NUEVO)</strong>.
                 </small>
                 <select id="ministerio_id" name="ministerio_id" class="controls" style="height: 5.573667rem;" required>
@@ -46,21 +47,23 @@
             </div>
             <br>
             <div class="form-group">
-                <label for="tel" class="text-primary-m">Edad</label>
+                <label for="tel" class="text-primary-m" style="color:rgba(0, 195, 244, 1) !important;">Edad</label>
                 <input type="text" id="edad" name="edad"
                     class="controls{{ $errors->has('edad') ? ' is-invalid' : '' }}" value="{{ old('edad') }}" required
                     placeholder="Ej: 20">
             </div>
             <br>
             <div class="form-group">
-                <label for="tel" class="text-primary-m">WhatsApp</label>
+                <label for="tel" class="text-primary-m"
+                    style="color:rgba(0, 195, 244, 1) !important;">WhatsApp</label>
                 <input type="text" id="tel" name="tel"
                     class="controls {{ $errors->has('tel') ? ' is-invalid' : '' }}" value="{{ old('tel') }}"
                     required placeholder="Ej: 987654321">
             </div>
             <br>
             <div class="form-group col-lg-6">
-                <label for="tel" class="text-primary-m">Captura de pago</label>
+                <label for="tel" class="text-primary-m" style="color:rgba(0, 195, 244, 1) !important;">Captura de
+                    pago</label>
                 <div class="file-upload">
                     <input type="file" class="file-input" id="foto" name="foto"
                         accept="image/jpeg, image/png" onchange="validateFile(this)">
@@ -72,7 +75,7 @@
                 .file-upload {
                     position: relative;
                     margin-top: 0.5rem;
-                    border: 2px dashed #007bff;
+                    border: 2px dashed rgba(0, 195, 244, 1);
                     border-radius: 0.25rem;
                     padding: 20px;
                     text-align: center;
@@ -85,14 +88,14 @@
                 }
 
                 .file-label {
-                    color: #007bff;
+                    color: rgba(0, 195, 244, 1);
                     font-weight: bold;
                     font-size: 30px;
                 }
 
                 .file-upload:hover {
                     background-color: #f1f1f1;
-                    border-color: #0056b3;
+                    border-color: rgba(0, 195, 244, 1);
                 }
 
                 .text-danger {
@@ -135,10 +138,10 @@
                 </div>
             </div>
             <br><br>
-            <div class="">
-                <button type="submit" class="btn-m btn-primary-gradient"><i class="fa fa-save"></i> Registrar
-                    Asistencia</button>
+            <div style="text-align: center;">
+                <button type="submit">Registrar</button>
             </div>
+
             <br>
             @if (session('success'))
                 <div class="alert alert-success">
@@ -179,7 +182,7 @@
         margin: 100px;
         padding: 40px;
         border-radius: 50px;
-        font-family: 'calibri';
+        font-family: 'calibri' !important;
         color: white;
         box-shadow: 7px 13px 37px rgba(0, 0, 0, 0.7);
     }
@@ -190,7 +193,7 @@
         padding: 10px;
         border-radius: 4px;
         margin-bottom: 16px;
-        border: 1px solid #1f53c5;
+        border: 1px solid rgba(0, 195, 244, 1);
         font-family: 'calibri';
         font-size: 28px;
         color: white;
@@ -201,5 +204,49 @@
         width: 100%;
         padding: 12px;
         font-size: 16px;
+    }
+
+    button {
+        background: rgba(0, 195, 244, 1);
+        color: #fff;
+        border: none;
+        position: relative;
+        height: 60px;
+        font-size: 1.6em;
+        padding: 0.1em 8em;
+        cursor: pointer;
+        transition: 800ms ease all;
+        outline: none;
+        border-radius: 50px;
+    }
+
+    button:hover {
+        background: #fff;
+        color: rgba(0, 195, 244, 1);
+    }
+
+    button:before,
+    button:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 2px;
+        width: 0;
+        background: rgba(0, 195, 244, 1);
+        transition: 400ms ease all;
+    }
+
+    button:after {
+        right: inherit;
+        top: inherit;
+        left: 0;
+        bottom: 0;
+    }
+
+    button:hover:before,
+    button:hover:after {
+        width: 100%;
+        transition: 800ms ease all;
     }
 </style>
