cf20f1f19124adbc8fe23e9aba7d6a0504fd9a15
GianSe08 <wongvasquezsebastian@gmail.com>
2025-02-24 15:30:02 -0500

    cambios

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -1,7 +1,7 @@
 {{-- @extends('app.index') --}}
 
 @section('styles')
-    {{-- <link rel="stylesheet" href="{{ asset('app/css/home/index.css') }}"> --}}
+{{-- <link rel="stylesheet" href="{{ asset('app/css/home/index.css') }}"> --}}
 @endsection
 
 @section('content')
@@ -12,7 +12,7 @@
 
     <div class="content_view_login">
         <div class="sect_login">
-            <div class="content_login">
+            <div class="content_login" style="width:50% !important;">
                 <div class="content_titulo_login">
                     <img src="{{ asset('app/img/logojacblack.png') }}" alt=""><br><br>
                     <span>BIENVENIDOS</span>
@@ -23,12 +23,13 @@
                     @csrf
                     <div class="form-group">
                         <label for="opinion" class="text-primary-m">Tu Opinión</label>
-                        <textarea id="opinion" name="opinion" class="form-control-m {{ $errors->has('opinion') ? ' is-invalid' : '' }}"
-                            required placeholder="Escribe tu opinión aquí">{{ old('opinion') }}</textarea>
+                        <textarea id="opinion" name="opinion"
+                            class="form-control-m {{ $errors->has('opinion') ? ' is-invalid' : '' }}" required
+                            placeholder="Escribe tu opinión aquí">{{ old('opinion') }}</textarea>
                         @if ($errors->has('opinion'))
-                            <span class="invalid-feedback" role="alert">
-                                <span style="color:#cd3232;">{{ $errors->first('opinion') }}</span>
-                            </span>
+                        <span class="invalid-feedback" role="alert">
+                            <span style="color:#cd3232;">{{ $errors->first('opinion') }}</span>
+                        </span>
                         @endif
                     </div>
                     <br>
@@ -47,9 +48,9 @@
                             <label for="star1" title="1 star">&#9733;</label>
                         </div>
                         @if ($errors->has('rating'))
-                            <span class="invalid-feedback" role="alert">
-                                <span style="color:#cd3232;">{{ $errors->first('rating') }}</span>
-                            </span>
+                        <span class="invalid-feedback" role="alert">
+                            <span style="color:#cd3232;">{{ $errors->first('rating') }}</span>
+                        </span>
                         @endif
                     </div>
                     <br>
@@ -57,18 +58,18 @@
                     <br><br>
                     {{-- Mensajes de éxito y error --}}
                     @if (session('success'))
-                        <div class="alert alert-success">
-                            <i class="fas fa-check-circle"></i> {{ session('success') }}
-                        </div>
+                    <div class="alert alert-success">
+                        <i class="fas fa-check-circle"></i> {{ session('success') }}
+                    </div>
                     @endif
                     @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                 </form>
 
@@ -77,29 +78,29 @@
     </div>
 </section>
 <style>
-    .rating {
-        direction: rtl;
-        display: inline-block;
-    }
+.rating {
+    direction: rtl;
+    display: inline-block;
+}
 
-    .rating input {
-        display: none;
-    }
+.rating input {
+    display: none;
+}
 
-    .rating label {
-        font-size: 30px;
-        color: #b9b9b9;
-        cursor: pointer;
-    }
+.rating label {
+    font-size: 30px;
+    color: #b9b9b9;
+    cursor: pointer;
+}
 
-    .rating input:checked~label {
-        color: #f7c04d;
-        /* Color for selected stars */
-    }
+.rating input:checked~label {
+    color: #f7c04d;
+    /* Color for selected stars */
+}
 
-    .rating label:hover,
-    .rating label:hover~label {
-        color: #f7c04d;
-        /* Hover color */
-    }
+.rating label:hover,
+.rating label:hover~label {
+    color: #f7c04d;
+    /* Hover color */
+}
 </style>
