d5da67754203d41a06ad7ba82e0481c13392760d
svasquez <svasquez@montgroup.com.pe>
2025-08-19 22:30:44 -0500

    update

--- resources/views/auth/inicio/index.blade.php
+++ resources/views/auth/inicio/index.blade.php
@@ -545,26 +545,7 @@ Highcharts.chart('otro', {
     }]
 });
 
-function transformBautizados(data) {
-    let arr = [];
 
-    if (Array.isArray(data)) {
-        // Caso: data = [{bautizado: 'SI', total: 43}, {bautizado: 'NO', total: 69}]
-        arr = data;
-    } else if (typeof data === 'object' && data !== null) {
-        // Caso: data = { SI: 43, NO: 69 }
-        arr = Object.keys(data).map(key => ({
-            bautizado: key,
-            total: data[key]
-        }));
-    }
-
-    // Transformamos al formato que Highcharts espera
-    return arr.map(item => ({
-        name: item.bautizado,
-        y: parseFloat(item.total)
-    }));
-}
 
 
 Highcharts.chart('bautizados', {
@@ -585,7 +566,7 @@ Highcharts.chart('bautizados', {
     series: [{
         name: 'Cantidad',
         colorByPoint: true,
-        data: transformBautizados(obtenerBautizados.data || obtenerBautizados)
+        data: transformData(obtenerBautizados.data || obtenerBautizados)
     }]
 });
 
